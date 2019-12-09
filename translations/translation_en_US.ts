866e35f613a7b77468fb15a4786cd37435d5b720
Aleksi Hiltunen <teemu.hiltunen@tut.fi>
2019-12-09 08:45:52 +0200

    imported beatbox app

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Peace</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Peace</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Peace</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Time to beat box. Would you be kind and give me a mic, please?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Time to beat box. Would you be kind and give me a mic, please?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Peace</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Peace</translation>
+        </message>
+    </context>
+</TS>
